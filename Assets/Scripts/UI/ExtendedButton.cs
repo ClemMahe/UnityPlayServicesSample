cd8c3f914e09a0411d450bede6578cb1529afda9
Clément MAHE <mahe.clem@gmail.com>
2020-11-11 15:20:41 +0100

    Solve warnings due to external dependencies

---
+++ Assets/Scripts/UI/ExtendedButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ExtendedButton : Button
+{
+
+    //public Color baseColor;
+    //public Color disabledTextColor;
+
+    void Update()
+    {
+        /*
+        Button button = this.GetComponent<Button>();
+        Text text = (Text)button.GetComponentInChildren<Text>();
+        if (this.currentSelectionState == SelectionState.Disabled){
+            text.color = disabledTextColor;
+        }else{
+            text.color = baseColor;
+        } */
+    } 
+}
+
