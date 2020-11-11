a154f54a3d647fce1d74421d60e4caa5e380221c
Clément MAHE <mahe.clem@gmail.com>
2020-11-11 15:37:10 +0100

    Remove useless Debug.log

--- Assets/Scripts/Global/SkyboxCameraScript.cs
+++ Assets/Scripts/Global/SkyboxCameraScript.cs
@@ -10,7 +10,6 @@ public class SkyboxCameraScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HelloClem");
     }
 
     // Update is called once per frame
